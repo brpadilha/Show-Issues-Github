cf01cad9da9ee5f4c8b6e6b45bf5552f5e67cf54
Brpadilha <brpadilha.dev@gmail.com>
2019-10-21 16:42:52 -0300

    Estiliza a página main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
-import {Title} from './styles'
+import {Container, Form, SubmitButton} from './styles'
+import { FaGithubAlt, FaPlus} from 'react-icons/fa'
+
 
 function Main(){
-  return <Title >Hello World </Title>
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt/>
+        Repositórios
+        </h1>
+        <Form onSubmit={()=>{}}>
+          <input type='text' placeholder="Adicionar repositório"></input>
+
+        <SubmitButton disabled>
+          <FaPlus color='#fff' size={14}/>
+        </SubmitButton>
+        </Form>
+    </Container>
+  )
 }
 export default Main
