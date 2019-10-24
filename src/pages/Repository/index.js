e3275776271e13dfbe0527bfcf27e52e16529a56
Brpadilha <brpadilha.dev@gmail.com>
2019-10-24 18:13:03 -0300

    Finaliza aplicação

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,84 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+import propTypes from 'prop-types'
+import api from '../../services/api'
+import { Loading, Owner, IssuesList } from './styles';
+import Container from '../../components/Container/index'
 
-// import { Container } from './styles';
 
-export default function Repository({match}) {
-  return <h1>Repository: {decodeURIComponent(match.params.repository)}</h1>
+export default class Repository extends Component{
+  static propTypes = {
+    match: propTypes.shape({
+      params: propTypes.shape({
+        repository: propTypes.string
+      })
+    }).isRequired
+  }
+
+
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  }
+  async componentDidMount(){
+    const{ match } = this.props;
+    const repoName = decodeURIComponent(match.params.repository);
+
+
+    const [repository, issues]= await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`,{
+        params: {
+          state: 'open',
+          per_page: 5,
+        }
+      })
+    ])
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false,
+    })
+
+  }
+  render(){
+    const {repository, issues, loading} = this.state
+
+
+    if (loading){
+      return <Loading>Carregando</Loading>
+    }
+    return (
+    <Container>
+      <Owner>
+        <Link to="/"> Voltar aos repositórios </Link>
+        <img src={repository.owner.avatar_url} alt={repository.owner.login}></img>
+        <h1>{repository.name}</h1>
+        <p>{repository.description}</p>
+
+      </Owner>
+
+      <IssuesList>
+        {issues.map(issue=>(
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login}/>
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+                {issue.labels.map(label =>(
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssuesList>
+
+    </Container>)
+
+  }
 }
+
